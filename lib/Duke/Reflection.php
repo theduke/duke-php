327ac576e6f6bef9f9dade5861c90aa283ec319a
theduke <christoph.herzog@theduke.at>
2011-05-06 13:32:55 +0200

    Added reflection helper.

---
+++ lib/Duke/Reflection.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Duke;
+
+class Reflection
+{
+	/**
+	 * Get a classes constants, optionally filter out constants not containing pattern
+	 * 
+	 * @param string|object $class
+	 * @param string $pattern
+	 */
+	public static function getClassConstants($class, $pattern=null)
+	{
+		if (is_object($class)) $class = get_class($class);
+		
+		$refl = new \ReflectionClass($class);
+		$constants = $refl->getConstants();
+		
+		if ($pattern)
+		{
+			$final = array();
+			
+			foreach ($constants as $key => $value)
+			{
+				if (strpos($key, $pattern) !== -1)
+				{
+					$final[$key] = $value;
+				}
+			}
+			
+			$constants = $final;
+		}
+		
+		return $constants;
+	}
+}
